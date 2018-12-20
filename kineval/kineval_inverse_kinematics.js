ff8ee3fc084dee44ca43e23ead9fc62dc2ce2d65
AndreaCY <43350550+AndreaCY@users.noreply.github.com>
2018-12-19 22:59:55 -0500

    rewrite for IK Eulerangle regrade - ASGN 5

--- kineval/kineval_inverse_kinematics.js
+++ kineval/kineval_inverse_kinematics.js
@@ -64,99 +64,118 @@ kineval.randomizeIKtrial = function randomIKtrial () {
 
 kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world, endeffector_joint, endeffector_position_local) {
 
-    // STENCIL: implement inverse kinematics iteration
-    var m_transfer = [];
-    var endeffector_xyz = [];
-    var endeffector_rpy = [];
-    var endeffector_worldposition = [];
+    var IK_link_chain = []; 
+    var joint_push = endeffector_joint;
+    var base_flag = false;
 
-    m_transfer = robot.joints[endeffector_joint].xform;
-    endeffector_xyz = matrix_multiply(m_transfer, endeffector_position_local);
-    endeffector_rpy = [[Math.atan2(m_transfer[2][1], m_transfer[2][2])], [Math.atan2(-m_transfer[2][0], Math.sqrt(Math.pow(m_transfer[2][1], 2)+Math.pow(m_transfer[2][2], 2)))], [Math.atan2(m_transfer[1][0], m_transfer[0][0])]];
-    endeffector_worldposition = [[endeffector_xyz[0][0]], [endeffector_xyz[1][0]], [endeffector_xyz[2][0]], [endeffector_rpy[0][0]], [endeffector_rpy[1][0]], [endeffector_rpy[2][0]]];
- 
-    var minix = [];
-    var a;   
-    for (a = 0; a< 6; a++){
-        minix[a] = [];
-        if (a < 3){
-            minix[a][0] = endeffector_target_world.position[a] - endeffector_worldposition[a][0];
+    while (!base_flag) {
+        IK_link_chain.push(joint_push);
+        if (robot.joints[joint_push].parent !== robot.base) {
+            joint_push = robot.links[robot.joints[joint_push].parent].parent; 
         }
         else{
-            if (kineval.params.ik_orientation_included){
-                minix[a][0] = endeffector_target_world.orientation[a-3] - endeffector_worldposition[a][0];
-            }
-            else{minix[a][0] = 0;}
+            base_flag = true;
         }
     }
 
-    // create jacobian_matrix
-    var jacobian_matrix = [];
-    var count_joint = 0;
-    var flag_jacobian = true;
-    var joint_onchain = endeffector_joint;
+    // Jacobian matrices, 6 x N intial size
+    J = [[],[],[],[],[],[]];    
+
+    // Parameters needed after
+    IK_chain_forward = IK_link_chain.slice();
+    IK_chain_forward.reverse();
+    endeffector_world = matrix_multiply(robot.joints[endeffector_joint].xform, endeffector_position_local);
+    target_world = matrix_copy(endeffector_target_world.position); 
+
+    for (var i=0; i<IK_chain_forward.length; i++) {
+        joint_origin_world = matrix_multiply(robot.joints[IK_chain_forward[i]].xform, [[0],[0],[0],[1]]);
+        joint_axis_local = [[robot.joints[IK_chain_forward[i]].axis[0]],[robot.joints[IK_chain_forward[i]].axis[1]],[robot.joints[IK_chain_forward[i]].axis[2]],[0]];
+        joint_axis_world = matrix_multiply(robot.joints[IK_chain_forward[i]].xform, joint_axis_local);
+        
+        // angular velocity
+        J[3][i] = joint_axis_world[0][0]; 
+        J[4][i] = joint_axis_world[1][0]; 
+        J[5][i] = joint_axis_world[2][0]; 
+
+
+        vec_origin_endeffector = vector_subtraction(endeffector_world, joint_origin_world);
+
+        J_cross = vector_cross(joint_axis_world, vec_origin_endeffector);
+        J[0][i] = J_cross[0]; 
+        J[1][i] = J_cross[1]; 
+        J[2][i] = J_cross[2];
+
+
+        if (robot.joints[ IK_chain_forward[i] ].type === 'prismatic') {
+            J[0][i] = joint_axis_world[0][0]; 
+            J[1][i] = joint_axis_world[1][0]; 
+            J[2][i] = joint_axis_world[2][0];
+            J[3][i] = 0;
+            J[4][i] = 0; 
+            J[5][i] = 0; 
+        }
+        
+    }
+
+    xform_matrix = matrix_copy(robot.joints[endeffector_joint].xform);
+    euler_angle = angle_from_rotation_matrix(xform_matrix);
     
-    // use a flag for determination
-    while (flag_jacobian){
-        if (robot.joints[joint_onchain].parent === robot.base){
-            flag_jacobian = false;
-        }
-        
-        var m_transfer_joint = [];
-        var world_joint_axis = [];
-        var i,j;
-        var world_joint_orig = [];
-        var origin_difference_joint = [];
-        var origin_difference_cro_axis = [];
-
-        //transfer joint matrix
-        for (i = 0; i< 3; i++){
-            m_transfer_joint[i] = [];
-            for (j = 0; j< 3; j++){
-                m_transfer_joint[i][j] = robot.joints[joint_onchain].xform[i][j];
-            }
-        }
-
-        //define axis of world joint using transfer joint matrix
-        world_joint_axis = matrix_multiply(m_transfer_joint, [[robot.joints[joint_onchain].axis[0]],[robot.joints[joint_onchain].axis[1]], [robot.joints[joint_onchain].axis[2]]]);
-        world_joint_axis = [world_joint_axis[0][0], world_joint_axis[1][0], world_joint_axis[2][0]];
-        world_joint_axis = vector_normalize(world_joint_axis);
-
-        if (robot.joints[joint_onchain].type === "prismatic"){
-            jacobian_matrix[count_joint] = [world_joint_axis[0], world_joint_axis[1], world_joint_axis[2], 0, 0, 0];
-        }
-        else{
-            world_joint_orig = matrix_multiply(robot.joints[joint_onchain].xform, [[0],[0],[0],[1]]);
-            origin_difference_joint = [endeffector_xyz[0][0] - world_joint_orig[0][0], endeffector_xyz[1][0] - world_joint_orig[1][0], endeffector_xyz[2][0] - world_joint_orig[2][0]];
-            origin_difference_cro_axis = vector_cross(world_joint_axis, origin_difference_joint);
-            jacobian_matrix[count_joint] = [origin_difference_cro_axis[0], origin_difference_cro_axis[1], origin_difference_cro_axis[2], world_joint_axis[0], world_joint_axis[1], world_joint_axis[2]];
-        }
-        
-        joint_onchain = robot.links[robot.joints[joint_onchain].parent].parent;
-        count_joint += 1;
+    var delta_position = vector_subtraction(endeffector_target_world.position, endeffector_world); // column vector
+    var delta_orientation = vector_subtraction(endeffector_target_world.orientation, euler_angle); // column vector
+    if (kineval.params.ik_orientation_included) {
+        dx = [[delta_position[0][0]],[delta_position[1][0]],[delta_position[2][0]],[delta_orientation[0]],[delta_orientation[1]],[delta_orientation[2]]];
+    }
+    else {
+        dx = [[delta_position[0][0]],[delta_position[1][0]],[delta_position[2][0]],[0],[0],[0]];        
     }
 
-    // Create the control
-    var control_matrix = [];
-    if (kineval.params.ik_pseudoinverse){
-        control_matrix = matrix_multiply(matrix_pseudoinverse(matrix_transpose(jacobian_matrix)), minix);
-    }
-    else{
-        control_matrix = matrix_multiply(jacobian_matrix, minix);
-    }
-    count_joint = 0;
-    flag_jacobian = true;
-    joint_onchain = endeffector_joint;
-    while (flag_jacobian){
-        if (robot.joints[joint_onchain].parent === robot.base){
-            flag_jacobian = false;
-        }
-        robot.joints[joint_onchain].control += kineval.params.ik_steplength * control_matrix[count_joint][0];
-        joint_onchain = robot.links[robot.joints[joint_onchain].parent].parent;
-        count_joint += 1;
-    }
+    if (kineval.params.ik_pseudoinverse)
+        dq = matrix_multiply(matrix_pseudoinverse(J), dx);
+    else
+        dq = matrix_multiply(matrix_transpose(J), dx);
+
+    for (var i=0; i<IK_chain_forward.length; i++)
+        robot.joints[IK_chain_forward[i]].control += kineval.params.ik_steplength * dq[i][0];
 
 }
 
+function angle_from_rotation_matrix(xform) {
 
+    var xform11 = xform[0][0], xform12 = xform[0][1], xform13 = xform[0][2];
+    var xform21 = xform[1][0], xform22 = xform[1][1], xform23 = xform[1][2];
+    var xform31 = xform[2][0], xform32 = xform[2][1], xform33 = xform[2][2];
+    var angle_x, angle_y, angle_z;
+    var EPS = 1e-5;
 
+    angle_y = Math.asin(Math.max(-1, Math.min(xform13, 1)));
+    if (Math.abs(xform13) < 1-EPS) {
+        angle_x = Math.atan2(-xform23, xform33);
+        angle_z = Math.atan2(-xform12, xform11);
+    }
+    else {
+        angle_x = Math.atan2(xform32, xform22);
+        angle_z = 0;
+    }
+    return [angle_x, angle_y, angle_z];
+}
+
+function vector_subtraction(v1, v2) {
+
+    var is_v1_col = !(typeof v1[0][0] === 'undefined');
+    var is_v2_col = !(typeof v2[0][0] === 'undefined');
+    var n = v1.length;
+    var vector_difference = [];
+
+    if (is_v1_col && is_v2_col) {
+        for(var i=0; i<n; i++) {
+            vector_difference[i] = [];
+            vector_difference[i][0] = v1[i][0] - v2[i][0];
+        }        
+    }
+    if (!is_v1_col && !is_v2_col) {
+        for(var i=0; i<n; i++) {
+            vector_difference[i] = v1[i] - v2[i];
+        }        
+    }
+    return vector_difference;
+}
